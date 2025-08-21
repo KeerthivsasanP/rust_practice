661c05c6d143339f3ad2f385d28b30f975eda7aa
24F1002528 <pkv1091@gmail.com>
2025-08-21 23:10:56 +0530

    hello world in rust

--- hello_world/hello_world.rs
+++ hello_world/hello_world.rs
@@ -1,6 +1,3 @@
-fn main() {
-    // Statements here are executed when the compiled binary is called.
-
-    // Print text to the console.
-    println!("Hello World!");
+fn main(){
+    println!("Hello world");
 }
